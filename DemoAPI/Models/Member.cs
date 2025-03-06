75a2def5d23f3515e7529f7acfe84a00925c23bf
siriwan limsakul <siriwan.limsakul@sato-global.com>
2025-03-06 15:17:40 +0700

    2025-03-06

---
+++ DemoAPI/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoAPI.Models
+{
+	public class Member
+	{
+		public int? Id { get; set; }
+		public string? FirstMame { get; set; }
+
+		public string? LastNmae { get; set; }
+
+		public string? Email { get; set; }
+
+		public DateTime DateOfBirth { get; set; }
+
+
+	}
+}
